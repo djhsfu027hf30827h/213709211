28690f219f9bf2fcb32d0a47ebaafe0f0e3225e9
djhsfu027hf30827h <123858175+djhsfu027hf30827h@users.noreply.github.com>
2023-02-03 21:31:50 -0600

    Update dihqwokjhdw.lua

--- dihqwokjhdw.lua
+++ dihqwokjhdw.lua
@@ -51,30 +51,35 @@ local Section = Tab:AddSection({
 	Name = "General"
 })
 
-
-function breakFunction(func)
-	for i,v in pairs(getconstants(func)) do
-		pcall(function()
-			setconstant(func, i, 'game.Workspace')
-		end)
-	end
-	for i,v in pairs(getupvalues(func)) do
-		pcall(function()
-			setconstant(func, i, game.Workspace)
-		end)
-	end
- end
- 
- 
- for i,v in pairs(getgc()) do
-	if type(v) == 'function' and islclosure(v) and not(is_synapse_function(v)) then
-		if string.match(getinfo(v).source, 'BubbleChat') then
-			breakFunction(v)
+Tab:AddButton({
+	Name = "remove ac",
+	Callback = function()
+        function breakFunction(func)
+			for i,v in pairs(getconstants(func)) do
+				pcall(function()
+					setconstant(func, i, 'game.Workspace')
+				end)
+			end
+			for i,v in pairs(getupvalues(func)) do
+				pcall(function()
+					setconstant(func, i, game.Workspace)
+				end)
+			end
+		 end
+		 
+		 
+		 for i,v in pairs(getgc()) do
+			if type(v) == 'function' and islclosure(v) and not(is_synapse_function(v)) then
+				if string.match(getinfo(v).source, 'BubbleChat') then
+					breakFunction(v)
+				end
+			end
+		
+			
 		end
-	end
+  	end    
+})
 
-	
-end
 
 Tab:AddTextbox({
 	Name = "Jump Power",

906d834b871e16f785cbc8186c63ea80216d40a6
djhsfu027hf30827h <123858175+djhsfu027hf30827h@users.noreply.github.com>
2023-02-03 21:05:14 -0600

    Update dihqwokjhdw.lua

--- dihqwokjhdw.lua
+++ dihqwokjhdw.lua
@@ -35,29 +35,7 @@ Tab:AddTextbox({
 
 		
 
-			function breakFunction(func)
-				for i,v in pairs(getconstants(func)) do
-					pcall(function()
-						setconstant(func, i, 'game.Workspace')
-					end)
-				end
-				for i,v in pairs(getupvalues(func)) do
-					pcall(function()
-						setconstant(func, i, game.Workspace)
-					end)
-				end
-			 end
-			 
-			 
-			 for i,v in pairs(getgc()) do
-				if type(v) == 'function' and islclosure(v) and not(is_synapse_function(v)) then
-					if string.match(getinfo(v).source, 'BubbleChat') then
-						breakFunction(v)
-					end
-				end
-
-				
-			end
+			
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
@@ -74,6 +52,29 @@ local Section = Tab:AddSection({
 })
 
 
+function breakFunction(func)
+	for i,v in pairs(getconstants(func)) do
+		pcall(function()
+			setconstant(func, i, 'game.Workspace')
+		end)
+	end
+	for i,v in pairs(getupvalues(func)) do
+		pcall(function()
+			setconstant(func, i, game.Workspace)
+		end)
+	end
+ end
+ 
+ 
+ for i,v in pairs(getgc()) do
+	if type(v) == 'function' and islclosure(v) and not(is_synapse_function(v)) then
+		if string.match(getinfo(v).source, 'BubbleChat') then
+			breakFunction(v)
+		end
+	end
+
+	
+end
 
 Tab:AddTextbox({
 	Name = "Jump Power",
